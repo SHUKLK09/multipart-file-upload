a71152d7f39357ad09dd29fc9a6f03aa3ed91cef
meenakshi-pathani <meenakshi.pathani@pfizer.com>
2022-10-12 13:25:57 +0530

    cleanup and update version

--- android/src/main/java/com/mycompany/plugins/example/FileUploadPlugin.java
+++ android/src/main/java/com/mycompany/plugins/example/FileUploadPlugin.java
@@ -15,15 +15,6 @@ public class FileUploadPlugin extends Plugin {
     private FileUpload implementation = new FileUpload();
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
-
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
-    }
-
-    @PluginMethod
     public void uploadFile(PluginCall call) {
         System.out.println(call.toString());
         try {
--- ios/Plugin/FileUpload.swift
+++ ios/Plugin/FileUpload.swift
@@ -3,54 +3,117 @@ import Capacitor
 
 @objc public class FileUpload: NSObject {
 
-
-    @objc func uploadFile(_ call : CAPPluginCall)throws {
+    @objc func uploadFile(_ call: CAPPluginCall) throws {
 
         debugPrint("uploadImage");
         // your image from Image picker, as of now I am picking image from the bundle
         guard let urlString = call.getString("url"),
-            let url = URL(string : urlString)
+              let url = URL(string: urlString)
         else {
             throw URLError(.badURL)
         }
 
         guard let filePath = call.getString("filePath"),
-        let docData = self.loadFileFromLocalPath(filePath)
+              let docData = self.loadFileFromLocalPath(filePath)
         else {
             return call.reject("Must provide a file path to upload file")
         }
 
-        var urlRequest = URLRequest(url : url)
+        var urlRequest = URLRequest(url: url)
 
-    let fileKey = call.getString("fileKey") ?? "file"
-    let headers = (call.getObject("headers") ?? [:])as ![String: String] let params = (call.getObject("params") ?? [:])as ![String: String] urlRequest.allHTTPHeaderFields = headers
-    urlRequest.httpMethod = "post"
+        let fileKey = call.getString("fileKey") ?? "file"
+        let headers = (call.getObject("headers") ?? [:]) as! [String: String]
+        let params = (call.getObject("params") ?? [:]) as! [String: String]
 
-    let bodyBoundary = "--------------------------\(UUID().uuidString)"
-    urlRequest.addValue("multipart/form-data; boundary=\(bodyBoundary)", forHTTPHeaderField : "Content-Type")
+        urlRequest.allHTTPHeaderFields = headers
+        urlRequest.httpMethod = "post"
 
-    let requestData = createBodyWithParameters(parameters : params, filePathKey : fileKey, imageDataKey : docData, boundary : bodyBoundary, filePath : filePath)
-    urlRequest.addValue("\(requestData.count)", forHTTPHeaderField : "content-length")
-    urlRequest.httpBody = requestData
+        let bodyBoundary = "--------------------------\(UUID().uuidString)"
+        urlRequest.addValue("multipart/form-data; boundary=\(bodyBoundary)", forHTTPHeaderField: "Content-Type")
 
-    URLSession.shared.dataTask(with : urlRequest) {
-        (data, httpUrlResponse, error) in
+        let requestData = createBodyWithParameters(parameters: params, filePathKey: fileKey, imageDataKey: docData, boundary: bodyBoundary, filePath: filePath)
+        urlRequest.addValue("\(requestData.count)", forHTTPHeaderField: "content-length")
+        urlRequest.httpBody = requestData
 
-        if error != nil {
-            return
-        
+        URLSession.shared.dataTask(with: urlRequest) { (data, httpUrlResponse, error) in
+
+            if error != nil {
+                return
+            }
+            call.resolve(self.handleResponse(data, httpUrlResponse as! HTTPURLResponse))
+        }.resume()
     }
-    call.resolve(self.handleResponse(data, httpUrlResponse as !HTTPURLResponse))
-}.resume()}private func handleResponse(_ data : Data ?, _ response: HTTPURLResponse) -> [String: Any]{var output = [:] as[String: Any] output["status"] = response.statusCode output["headers"] = response.allHeaderFields output["url"] = response.url ?. absoluteString guard let data = data else {
-    output["data"] = ""
-    return output
-} let contentType = (response.allHeaderFields["Content-Type"] as ? String ?? "application/default").lowercased();
 
-if (contentType.contains("application/json")) {
-    output["data"] = parseJson(data);
+    private func handleResponse(_ data: Data?, _ response: HTTPURLResponse) -> [String:Any] {
+        var output = [:] as [String:Any]
+
+        output["status"] = response.statusCode
+        output["headers"] = response.allHeaderFields
+        output["url"] = response.url?.absoluteString
+
+        guard let data = data else {
+            output["data"] = ""
+            return output
+        }
+
+        let contentType = (response.allHeaderFields["Content-Type"] as? String ?? "application/default").lowercased();
+
+        if (contentType.contains("application/json") ) {
+            output["data"] = parseJson(data);
+        }
+
+        return output
+    }
+
+    private func parseJson(_ data: Data) -> Any {
+        do {
+            return try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+        } catch {
+            return error.localizedDescription
+        }
+    }
+
+    private func loadFileFromLocalPath(_ localFilePath: String) -> Data ? {
+        // let updatedPath = localFilePath.replacingOccurrences(of: "file:///", with: "")
+        guard let url = URL(string: localFilePath)else {debugPrint("Failed to get file data")
+            return nil
+        }
+        do {
+            let data = try Data(contentsOf: url)
+            return data
+        } catch {
+            debugPrint(error)debugPrint("Failed to get file data")return nil
+        }
+    }
+
+    private func createBodyWithParameters(parameters: [String: String], filePathKey: String?, imageDataKey: Data?, boundary: String, filePath: String) -> Data {
+        var body = Data();
+
+        for (key, value) in parameters {
+            body.appendString("--\(boundary)\r\n")
+            body.appendString("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n")
+            body.appendString("\(value)\r\n")
+        }
+
+        let filename = URL(fileURLWithPath: filePath).lastPathComponent
+        let mimetype = URL(string: filePath)
+
+        body.appendString("--\(boundary)\r\n")
+
+        if let filePath = filePathKey, let imageData = imageDataKey {
+            body.appendString("Content-Disposition: form-data; name=\"\(filePath)\"; filename=\"\(filename)\"\r\n")
+            body.appendString("Content-Type: \(mimetype)\r\n\r\n")
+            body.append(imageData)
+            body.appendString("\r\n")
+            body.appendString("--\(boundary)--\r\n")
+        }
+        return body
+    }
 }
 
-return output}private func parseJson(_ data: Data) -> Any {do {
-return try JSONSerialization.jsonObject(with
-: data, options
-:.mutableContainers)} catch {return error.localizedDescription}}private func loadFileFromLocalPath(_ localFilePath: String) -> Data ? { // let updatedPath = localFilePath.replacingOccurrences(of: "file:///", with: "")guard let url = URL(string: localFilePath)else {debugPrint("Failed to get file data")return nil}do {let data = try Data(contentsOf: url)return data} catch {debugPrint(error)debugPrint("Failed to get file data")return nil}}private func createBodyWithParameters(parameters: [String: String], filePathKey: String ?, imageDataKey: Data ?, boundary: String, filePath: String) -> Data {var body = Data();for (key, value)  in parameters {body.appendString("--\(boundary)\r\n")body.appendString("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n")body.appendString("\(value)\r\n")}let filename = URL(fileURLWithPath: filePath).lastPathComponentlet mimetype = URL(string: filePath)body.appendString("--\(boundary)\r\n")if let filePath = filePathKey, let imageData = imageDataKey {body.appendString("Content-Disposition: form-data; name=\"\(filePath)\"; filename=\"\(filename)\"\r\n") body.appendString("Content-Type: \(mimetype)\r\n\r\n")body.append(imageData)body.appendString("\r\n")body.appendString("--\(boundary)--\r\n")}return body}}extension Data {mutating func appendString(_ string: String) {let data = string.data(using: String.Encoding.utf8, allowLossyConversion: true)append(data !)}}
+extension Data {
+    mutating func appendString(_ string: String) {
+        let data = string.data(using: String.Encoding.utf8, allowLossyConversion: true)
+        append(data!)
+    }
+}
